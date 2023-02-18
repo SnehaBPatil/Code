8f069a6b6b4ef375e8972af32ac72d668fbee914
SNEHA B PATIL <83591501+SnehaBPatil@users.noreply.github.com>
2023-02-19 00:05:54 +0530

    Create given string can be arranged as palindrome.java

---
+++ given string can be arranged as palindrome.java
@@ -0,0 +1,20 @@
+  static boolean canFormPalindrome(String str)
+    {
+        int count[] = new int[256];
+        Arrays.fill(count, 0);
+ 
+   
+        for (int i = 0; i < str.length(); i++)
+            count[(int)(str.charAt(i))]++;
+ 
+        int odd = 0;
+        for (int i = 0; i < 256; i++) {
+            if ((count[i] & 1) == 1)
+                odd++;
+ 
+            if (odd > 1)
+                return false;
+        }
+ 
+        return true;
+    }
