3b720b2ea227af2a49986ea8323fb5de961e2bfa
SNEHA B PATIL <83591501+SnehaBPatil@users.noreply.github.com>
2023-03-11 23:26:26 +0530

    Create Reverse Integer.java

---
+++ Reverse Integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+       long num=0;
+        while(x!=0)
+        {
+            num=num*10+(x%10);
+            x=x/10;
+        }
+         if(!(-2147483648 <=num && num<=2147483647))
+            return 0;
+        else
+        return (int)num;
+    }
+}
