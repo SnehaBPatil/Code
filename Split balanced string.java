ed0e5de6c02200ee7dc7c2287a1e738ff47b7061
SNEHA B PATIL <83591501+SnehaBPatil@users.noreply.github.com>
2023-03-25 22:57:18 +0530

    Create Split balanced string.java

---
+++ Split balanced string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count =0;
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='R') sum1++;
+            if(s.charAt(i)=='L') sum2++;
+            if(sum1==sum2){
+                count++;
+                
+            }
+        }return count;
+    }
+}
